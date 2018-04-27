e28fc422c406d0c53b59a688b1bbe49106205379
sitehosters <38578542+sitehosters@users.noreply.github.com>
2018-04-27 20:04:32 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Includes
-  source $VESTA/func/main.sh
-  source $VESTA/conf/vesta.conf 
+source $VESTA/func/main.sh
+source $VESTA/conf/vesta.conf 
+
   cd /tmp
   curl -LOk https://github.com/sitehosters/oneclickinstall/releases/download/beta/oneclickinstall.zip
   unzip -o oneclickinstall.zip -d /usr/local/vesta
