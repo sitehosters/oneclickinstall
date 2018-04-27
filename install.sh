a721f05a1d0c57cc78b55aa280cdf89bac931a94
sitehosters <38578542+sitehosters@users.noreply.github.com>
2018-04-27 19:53:13 +0530

    install.sh
    Execute

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Includes
+  source $VESTA/func/main.sh
+  source $VESTA/conf/vesta.conf 
+
+
+  cd /tmp
+  curl -LOk https://github.com/sitehosters/oneclickinstall/releases/download/beta/oneclickinstall.zip
+  unzip -o oneclickinstall.zip -d /usr/local/vesta
+  rm -rf oneclickinstall.zip
+  chmod 755 $VESTA/bin/v-app
+   
+   echo "********************************************"
+   echo "********************************************"
+   echo "********************************************"
+   echo "One Click Installer has been installed successfully........"
+   echo "********************************************"
+   echo "********************************************"
+   echo "********************************************"
+   
+  exit
